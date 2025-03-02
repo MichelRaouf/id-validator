12a7a1ffb1ad9611c36af4505a0e36e078416cd3
Michel Iskander <michel.raouf00@gmail.com>
2025-03-02 13:20:42 +0200

    modified ID extraction logic

--- id_validator/utils.py
+++ id_validator/utils.py
@@ -4,7 +4,11 @@ from datetime import datetime
 GOVERNORATES = {
     "01": "Cairo", "02": "Alexandria", "03": "Port Said", "04": "Suez",
     "11": "Damietta", "12": "Dakahlia", "13": "Sharqia", "14": "Qalyubia",
-    # Add more governorates as needed
+    "15": "Kafr El Sheikh", "16": "Gharbia", "17": "Monufia", "18": "Beheira",
+    "19": "Ismailia", "21": "Giza", "22": "Beni Suef", "23": "Fayoum",
+    "24": "Minya", "25": "Assiut", "26": "Sohag", "27": "Qena",
+    "28": "Aswan", "29": "Luxor", "31": "Red Sea", "32": "New Valley",
+    "33": "Matruh", "34": "North Sinai", "35": "South Sinai", "88": "Outside Egypt"
 }
 
 def validate_national_id(nid):
@@ -14,8 +18,10 @@ def validate_national_id(nid):
     century_code = nid[0]
     birth_date_str = nid[1:7]
     governorate_code = nid[7:9]
+    serial_number = nid[9:13]
+    gender_digit = int(nid[12])
+    check_digit = nid[13]
 
-    # Determine birth year
     if century_code == "2":
         birth_year = "19" + birth_date_str[:2]
     elif century_code == "3":
@@ -23,16 +29,19 @@ def validate_national_id(nid):
     else:
         return False, "Invalid century code in ID."
 
-    # Validate birthdate
     try:
         birth_date = datetime.strptime(birth_year + birth_date_str[2:], "%Y%m%d")
     except ValueError:
         return False, "Invalid birth date in ID."
 
-    # Get governorate
     governorate = GOVERNORATES.get(governorate_code, "Unknown")
+
+    gender = "Male" if gender_digit % 2 == 1 else "Female"
 
     return True, {
         "birth_date": birth_date.strftime("%Y-%m-%d"),
-        "governorate": governorate
+        "governorate": governorate,
+        "gender": gender,
+        "serial_number": serial_number,
+        "check_digit": check_digit
     }
